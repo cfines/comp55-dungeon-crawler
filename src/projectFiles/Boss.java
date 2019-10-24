c76ca09f1835be076322a7e8fed090fd70632bea
Stanley <stanyu123@gmail.com>
2019-10-24 16:25:32 -0700

    added boss types to the boss class

--- src/projectFiles/Boss.java
+++ src/projectFiles/Boss.java
@@ -2,12 +2,12 @@ package projectFiles;
 
 public class Boss {
 	private Stats bossStats;
+	private ElementType bossType;
 	
-	Boss(int input_HP_cur, int input_HP_tot, int atkTime, int input_dmg, int input_x, int input_y){
-		bossStats = new Stats(input_HP_cur,input_HP_tot,atkTime,input_dmg, input_x, input_y);
+	Boss(int input_HP_cur, int input_HP_tot, int atkTime,
+			int input_dmg, int input_x, int input_y, ElementType bossType){
+		bossStats = new Stats(input_HP_cur, input_HP_tot, atkTime, input_dmg, input_x, input_y);
+		this.bossType = bossType;
 	}
 	
-	/*Boss(){
-		
-	}*/
 }
