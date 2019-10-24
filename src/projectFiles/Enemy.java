1328438ce62909d60021d33db1b7451a4da22b51
Stanley <stanyu123@gmail.com>
2019-10-24 16:31:00 -0700

    Created enemy class and the defualt constructor for that class

---
+++ src/projectFiles/Enemy.java
@@ -0,0 +1,12 @@
+package projectFiles;
+
+public class Enemy {
+	private Stats enemyStats;
+	private ElementType enemyType;
+	
+	Enemy (int input_HP_cur, int input_HP_tot, int atkTime,
+			int input_dmg, int input_x, int input_y, ElementType bossType){
+		enemyStats = new Stats(input_HP_cur, input_HP_tot, atkTime, input_dmg, input_x, input_y);
+		this.enemyType = bossType;
+	}
+}
