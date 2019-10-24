c57429733a6d2d645898d043dbeca27157d09222
Stanley <stanyu123@gmail.com>
2019-10-24 16:17:33 -0700

    Created the default constructor for the boss class

--- src/projectFiles/Boss.java
+++ src/projectFiles/Boss.java
@@ -1,5 +1,13 @@
 package projectFiles;
 
 public class Boss {
-
+	private Stats bossStats;
+	
+	Boss(int input_HP_cur, int input_HP_tot, int atkTime, int input_dmg, int input_x, int input_y){
+		bossStats = new Stats(input_HP_cur,input_HP_tot,atkTime,input_dmg, input_x, input_y);
+	}
+	
+	/*Boss(){
+		
+	}*/
 }
