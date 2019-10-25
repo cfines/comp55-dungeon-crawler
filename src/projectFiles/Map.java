bd4e1935a1a04a0e5d206f678ce1accc1136d782
Alan <a_barragan1@u.pacific.edu>
2019-10-24 17:01:10 -0700

    added instance variable for roomList in Map

--- src/projectFiles/Map.java
+++ src/projectFiles/Map.java
@@ -1,8 +1,14 @@
+//This the map class that takes in the values of map layout and room, 
+//spits out the combination, 
+//and leads to something beautiful
 package projectFiles;
+
+import java.util.ArrayList;
 
 public class Map {
 	private MapLayout type;
-
+	private ArrayList<Room> roomList;
+	
 	public Map()
 	{
 		
