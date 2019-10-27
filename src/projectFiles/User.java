2b2983ac08a844221b6f626916f118e1b99cdd6d
Scharkey <jordanscharkey@gmail.com>
2019-10-27 15:42:38 -0700

    Created constructors for Weapon and User
    -General and Default constructors created for both classes

--- src/projectFiles/User.java
+++ src/projectFiles/User.java
@@ -1,5 +1,59 @@
 package projectFiles;
 
-public class User {
+import java.util.ArrayList;
 
+public class User {
+	//INSTANCE VARIABLES//
+	
+	//Most stats
+	private Stats userStats;
+	
+	//User-specific stats
+	private int moveSpeedStat;
+	private double powerStat;
+	private int atkSpeedStat;
+	
+	//Weapon Inventory variables
+	private int arrSize;
+	private int arrEquiped;
+	private ArrayList<Weapon> weapons;
+	
+	//Other user-specific variables
+	private int numConsumable;
+	private boolean hasKey;
+	
+	///////////////////////
+	
+	//Default Constructor
+	User(){
+		moveSpeedStat = 1;
+		powerStat = 1;
+		atkSpeedStat = 1;
+		
+		arrSize = 3;
+		arrEquiped = 0;
+		weapons = new ArrayList<>(arrSize);
+		
+		numConsumable = 0;
+		hasKey = false;
+	}
+	
+	//General Constructor
+	User(int input_HP_cur, int input_HP_tot, int atkTime,
+			int input_dmg, int input_x, int input_y){
+		
+		userStats = new Stats(input_HP_cur, input_HP_tot, atkTime, input_dmg, input_x, input_y);
+		moveSpeedStat = 1;
+		powerStat = 1;
+		atkSpeedStat = 1;
+		
+		arrSize = 3;
+		arrEquiped = 0;
+		weapons = new ArrayList<>(arrSize);
+		
+		numConsumable = 0;
+		hasKey = false;
+		
+	}
+	
 }
--- src/projectFiles/Weapon.java
+++ src/projectFiles/Weapon.java
@@ -3,6 +3,14 @@ package projectFiles;
 public class Weapon {
 	private ElementType wepType;
 	
+	Weapon(){
+		wepType = null;
+	}
+	
+	Weapon(ElementType type){
+		wepType = type;
+	}
+	
 	public ElementType getWepType() {
 		return wepType;
 	}
