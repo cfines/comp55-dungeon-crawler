5982a7d6b31ec14c3f804ba98d66441acdda78e9
Alan <a_barragan1@u.pacific.edu>
2019-10-27 18:14:42 -0700

    created default getters and setters for some instance variables

--- src/projectFiles/Map.java
+++ src/projectFiles/Map.java
@@ -6,14 +6,39 @@ package projectFiles;
 import java.util.ArrayList;
 
 public class Map {
-	//instance variables
 	private MapLayout type;
 	private ArrayList<Room> roomList;
 	private Room curRoom;
 	
+	//instance variables
+	
 	public Map()
 	{
 		
+	}
+
+	public MapLayout getType() {
+		return type;
+	}
+
+	public void setType(MapLayout type) {
+		this.type = type;
+	}
+
+	public ArrayList<Room> getRoomList() {
+		return roomList;
+	}
+
+	public void setRoomList(ArrayList<Room> roomList) {
+		this.roomList = roomList;
+	}
+
+	public Room getCurRoom() {
+		return curRoom;
+	}
+
+	public void setCurRoom(Room curRoom) {
+		this.curRoom = curRoom;
 	}
 	
 	
