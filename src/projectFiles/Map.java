20ba78ab741e845a2abb4acf9b69ce9ca0ad12fe
Alan <a_barragan1@u.pacific.edu>
2019-10-27 19:18:38 -0700

    adjusted some instance variables along with the introduction of a couple more methods for map

--- src/projectFiles/Map.java
+++ src/projectFiles/Map.java
@@ -4,17 +4,22 @@
 package projectFiles;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
-public class Map {
+public class Map{
 	private MapLayout type;
+	private Interactions entries;
 	private ArrayList<Room> roomList;
 	private Room curRoom;
+	private HashMap <Room, spawnCoords> randSpawns;
+	private ArrayList<randSpawns> roomSpawns;
+	private HashMap <Interactions, Room> roomReact; 
 	//instance variables
 	
-	
-	public Map()
+	Map()
 	{
-		
+		int xCoord = 0;
+		int yCoord = 0;
 	}
 
 	public MapLayout getType() {
@@ -40,6 +45,37 @@ public class Map {
 	public void setCurRoom(Room curRoom) {
 		this.curRoom = curRoom;
 	}
-	
+
+	public HashMap <Room, spawnCoords> getRandSpawns() {
+		return randSpawns;
+	}
+
+	public void setRandSpawns(HashMap <Room, spawnCoords> randSpawns) {
+		this.randSpawns = randSpawns;
+	}
+
+	public ArrayList<randSpawns> getRoomSpawns() {
+		return roomSpawns;
+	}
+
+	public void setRoomSpawns(ArrayList<randSpawns> roomSpawns) {
+		this.roomSpawns = roomSpawns;
+	}
+
+	public HashMap <Interactions, Room> getRoomReact() {
+		return roomReact;
+	}
+
+	public void setRoomReact(HashMap <Interactions, Room> roomReact) {
+		this.roomReact = roomReact;
+	}
+
+	public Interactions getEntries() {
+		return entries;
+	}
+
+	public void setEntries(Interactions entries) {
+		this.entries = entries;
+	}
 	
 }
