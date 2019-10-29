f342a7d1959152667bbaf1b4403bfa1c764fb5fc
cfine <c_fines@u.pacific.edu>
2019-10-28 17:38:26 -0700

    successful test with hard coded entry points without a for look in MapLayout

--- src/projectFiles/MapLayout.java
+++ src/projectFiles/MapLayout.java
@@ -12,24 +12,26 @@ public class MapLayout {
 	private HashMap <String, String> mapHashMatrix;
 	private HashMap <String, String> mapHashEspeciallyThisOne;
 	
-	private HashMap <String, String> mapHashTest;
-	private int entryAmountofTest = 4;
-	public ArrayList<String> entryAmount;
+	private HashMap <String, String> mapHashTest = new HashMap <String, String>();
+	private int entryAmountofTest = 5;
+	public ArrayList<String> entryAmount = new ArrayList<String>();
+	public String test;
 	
 	MapLayout(){
 		roomID = "";
 		entryID = "";
+		test = "";
+		
 	}
 	
 	// returns mapHashTest with hard coded values for the sake of testing 
 	// will be changed later
 	public HashMap<String, String> getMapHash() {
-		mapHashTest.put(entryAmount.get(0), entryAmount.get(1)); 
-		mapHashTest.put(entryAmount.get(1), entryAmount.get(0)); 
-		mapHashTest.put(entryAmount.get(2), entryAmount.get(3)); 
-		mapHashTest.put(entryAmount.get(3), entryAmount.get(2)); 
-		mapHashTest.put(entryAmount.get(3), entryAmount.get(4));
 		return mapHashTest;
+	}
+	
+	public void setMapHash() {
+		getMapHash().put(entryAmount.get(0), entryAmount.get(1)); 
 	}
 	
 	/* TODO: connect class to the superclass Floor in order to recieve what mapLayout the player will be on, and
@@ -39,22 +41,25 @@ public class MapLayout {
 	
 	// based on the mapLayout, this for loop 
 	public void entryAmount() {
-		for(int i = 0; i >= entryAmountofTest; i++) {
-			entryAmount.add("E" + 1);
-		}
+		 /*for(int i = 0; i >= entryAmountofTest; i++) {
+			String temp = "E" + i;
+			entryAmount.add(temp);
+		 } */
+		entryAmount.add("E0");
+		entryAmount.add("E1");
 	}
 	
 	public void runTest() {
-		String test = entryAmount.get(0);
-		getMapHash().get(test);
-		System.out.println("User enters E1, the HashMap should spit out where the user will pop out from which shoudl be E2. Does it?:    "
-				+ test);
+		test = entryAmount.get(0);
+		System.out.println("User enters E0, the HashMap should spit out where the user will pop out from which shoudl be E1. Does it?:    "
+				+ getMapHash().get(test));
 	}
 	
-	public void main(){
+	public static void main(String[] args){
 		
-		entryAmount();
 		MapLayout testt = new MapLayout();
+		testt.entryAmount();
+		testt.setMapHash();
 		testt.runTest();
 		
 			
