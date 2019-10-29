d017fad3835216df65debd2fcd394d42338e47b8
Alan <a_barragan1@u.pacific.edu>
2019-10-28 19:03:35 -0700

    Created Floor class (waiting on PowerUps class to be created for full implementation)

---
+++ src/projectFiles/Floor.java
@@ -0,0 +1,18 @@
+package projectFiles;
+
+public class Floor {
+
+	private Map curMap;
+	//private PowerUps key;
+	//private PowerUps drop;
+	
+	private boolean decideDrop() 
+	{
+		return false;
+	}
+	
+	private void placeKey() 
+	{
+		
+	}
+}
