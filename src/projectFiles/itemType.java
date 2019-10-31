a60a1764208f78accafb2e7126eadb1949507842
Stanley <stanyu123@gmail.com>
2019-10-30 18:10:24 -0700

    finished adding elements to itemType enum

--- src/projectFiles/itemType.java
+++ src/projectFiles/itemType.java
@@ -1,5 +1,16 @@
 package projectFiles;
 
 public enum itemType {
-
+HPRECOVERY, ATKSPEED, MOVESPEED, POWERSTAT, HPINCREASE;
+	
+	public String toString() {
+		switch(this) {
+			case HPRECOVERY: return "active_hprecovery";
+			case ATKSPEED: return "passive_atkspeed";
+			case MOVESPEED: return "passive_movespeed";
+			case POWERSTAT: return "passive_powerstat";
+			case HPINCREASE: return "passive_hpincrease";
+		}
+		return "n/a";
+	}
 }
