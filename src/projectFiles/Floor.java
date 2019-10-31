282a7df0c1b915fbaa3f5d4b594fa0a391f55d99
cfine <c_fines@u.pacific.edu>
2019-10-30 18:56:11 -0700

    successfully tested the level counter and the map layout based on the level the user will be on yeah booooooiiiiiiii

--- src/projectFiles/Floor.java
+++ src/projectFiles/Floor.java
@@ -1,5 +1,7 @@
 package projectFiles;
 
+import java.util.ArrayList;
+
 public class Floor {
 
 	private Map curMap;
@@ -17,5 +19,58 @@ public class Floor {
 	private void placeKey() 
 	{
 		
+	}
+	
+	private int levelCounter = 0;
+	private ArrayList<String> map = new ArrayList<String>();
+	// this would need to be one of the FIRST functions to be called in order to initialize the arraylist of the types of maps
+	public void setMapArrayList() {
+		map.add("map_base1"); 
+		map.add("map_fire");
+		map.add("map_water");
+		map.add("map_earth");
+		map.add("map_base2");
+	}
+	// this will be called when the user advances onto the next level i.e. enters a staircase
+	public void levelAdder() {
+		levelCounter++;
+	}
+	
+	public String currentMap(String curMap) {
+		return curMap;
+	}
+	// returns the level counter
+	public int getLevelCounter() {
+		return levelCounter + 1;
+	}
+	
+	public String whatMapWeOn(){
+		int temp = levelCounter % 5;
+		return map.get(temp);
+	}
+	
+	//I'm adding a +1 to the getLevel counter since its offset by the ArrayList's first index being 0
+	public static void main(String[] args) {
+		Floor test = new Floor();
+		
+		test.setMapArrayList();
+		
+		System.out.println("We should currently be on level 1. Are we?: " + test.getLevelCounter());
+		System.out.println("Because we are on level " + test.getLevelCounter() + ", we should be on map_base1. Are we?: " + test.whatMapWeOn() + "\n");
+		
+		test.levelAdder();
+		
+		System.out.println("We should now be on level 2. Are we?: " + test.getLevelCounter());
+		System.out.println("Because we are on level " + test.getLevelCounter() + ", we should be on map_fire. Are we?: " + test.whatMapWeOn() + "\n");
+			
+		test.levelAdder();
+		test.levelAdder();
+		test.levelAdder();
+		test.levelAdder();
+		test.levelAdder();
+		
+		System.out.println("The player has advanced another 5 levels. It should now say we are on level 7. Does it?: " + test.getLevelCounter());
+		System.out.println("Because we are on level " + test.getLevelCounter() + ", we should be on map_fire again, "
+				+ "because we have done a full rotation of the map order. Are we?: " + test.whatMapWeOn() + "\n");
 	}
 }
