93fb1bc218f6a54021305cdcf1c435ff34b98182
Stanley <stanyu123@gmail.com>
2019-10-31 23:30:36 -0700

    Added key to the powerup class and a TODO for User

--- src/projectFiles/Console.java
+++ src/projectFiles/Console.java
@@ -1,5 +1,25 @@
 package projectFiles;
 
-public class Console {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
+import acm.program.GraphicsProgram;
+
+public class Console extends GraphicsProgram implements KeyListener{
+	private Map map;
+	private Room room;
+	
+	public static void main(String[] args) {
+		Console cg = new Console();
+		cg.playGame();
+	}
+	
+	public void playGame() {
+		addKeyListeners();
+			
+	}
+	
+	public void keyPressed(KeyEvent e) {
+		
+	}
 }
