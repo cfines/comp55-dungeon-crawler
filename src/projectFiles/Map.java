ed37aa7b26be3a7077d260520d239f0b2a15d24c
Alan <a_barragan1@u.pacific.edu>
2019-11-01 14:33:54 -0700

    made changes

--- src/projectFiles/Map.java
+++ src/projectFiles/Map.java
@@ -13,27 +13,31 @@ public class Map{
 	private static Enemy badGuy;
 	private static Boss biggerBadGuy;
 	private static Room curRoom;
+	private static Coordinates location;
+	private static Interactions react;
 	private static ArrayList<Room> roomList = new ArrayList<Room>();
 	private static ArrayList<Coordinates> roomSpawns = new ArrayList<Coordinates>();
 	private static HashMap <Room, ArrayList<Coordinates>> randSpawns = new HashMap<Room, ArrayList<Coordinates>>();
-	private static HashMap <Interactions, ArrayList<Room>> roomReact = new HashMap<Interactions,ArrayList<Room>>();
-	private static HashMap <Enemy, ArrayList<Coordinates>> enemySpawn = new HashMap <Enemy, ArrayList<Coordinates>>();
-	private static HashMap <Boss, ArrayList<Coordinates>> bossSpawn = new HashMap <Boss, ArrayList<Coordinates>>();
+	private static HashMap <Room, Interactions> roomReact = new HashMap<Room, Interactions>();
+	private static HashMap <Enemy, Coordinates> enemySpawn = new HashMap <Enemy, Coordinates>();
+	private static HashMap <Boss, Coordinates> bossSpawn = new HashMap <Boss, Coordinates>();
 	//TODO think of some alternative to put enemies onto a room and avoid making a 
 	//HashMap <Room, HashMap<Enemy, ArrayList<Coordinates> thing
 	
 	//basic constructor
 	Map()
-	{}
+	{
+		
+	}
 	
 	//sets the interaction to a room
-	public static void setRoomInteractions(Interactions react, ArrayList<Room> r) 
+	public static void setRoomInteractions(Interactions react, Room r) 
 	{
-		roomReact.put(react, r);
+		roomReact.put(r, react);
 	}
 	
 	//gets the hash map of the room interactions
-	public static HashMap<Interactions, ArrayList<Room>> getRoomInteractions()
+	public static HashMap<Room, Interactions> getRoomInteractions()
 	{
 		return roomReact;
 	}
@@ -88,17 +92,17 @@ public class Map{
 	}
 	
 	// gets coordinates from the enemy
-	public static HashMap <Enemy, ArrayList<Coordinates>> getEnemySpawn() {
+	public static HashMap<Enemy, Coordinates> getEnemySpawn() {
 		return enemySpawn;
 	}
 	
 	// sets enemy onto an array list of coordinates
-	public static void setEnemySpawn(HashMap <Enemy, ArrayList<Coordinates>> enemySpawn) {
-		enemySpawn.put(badGuy, roomSpawns);
+	public static void setEnemySpawn(Enemy enemy, Coordinates h) {
+		enemySpawn.put(badGuy, location);
 	}
 	
 	// gets coordinates from the boss
-	public static HashMap <Boss, ArrayList<Coordinates>> getBossSpawn() {
+	public static HashMap<Boss, Coordinates> getBossSpawn() {
 		return bossSpawn;
 	}
 	
@@ -108,23 +112,33 @@ public class Map{
 	}
 	//TODO fix the other functions to properly do what they need to do
 	
+	public static void somethingThatWouldMakeSense(String args[]) 
+	{
+		Coordinates danger = new Coordinates(300,500);
+		Coordinates inTheWay = new Coordinates (250,700);
+		setEnemySpawn(badGuy, danger);
+		setRoomSpawns(danger, roomSpawns);	
+		react = new Interactions(interactionType.obstacle_rock);
+		
+	}
+	
 	//big boi testing right here
 	public static void main (String args[]) 
 	{
 		
 		//test string to see if this actually wants to work for me
-		System.out.println("Sample test for Coordinate class compatibility with Map\n");
+//		System.out.println("Sample test for Coordinate class compatibility with Map\n");
 		
 		//test successful
-		Coordinates test1 = new Coordinates(50,76);
-		Coordinates test2 = new Coordinates(60,88);
-		setRoomSpawns(test1, roomSpawns);
-		getRoomSpawns();
-		System.out.println("Now testing with another variable being added, will it save both?");
-		setRoomSpawns(test2, roomSpawns);
-		System.out.println("It should be added if this text is even appearing, let us see the info again."
-			+ " Did it change?\n");  
-		getRoomSpawns();
+//		Coordinates test1 = new Coordinates(50,76);
+//		Coordinates test2 = new Coordinates(60,88);
+//		setRoomSpawns(test1, roomSpawns);
+//		getRoomSpawns();
+//		System.out.println("Now testing with another variable being added, will it save both?");
+//		setRoomSpawns(test2, roomSpawns);
+//		System.out.println("It should be added if this text is even appearing, let us see the info again."
+//			+ " Did it change?\n");  
+//		getRoomSpawns();
 		
 	}
 
