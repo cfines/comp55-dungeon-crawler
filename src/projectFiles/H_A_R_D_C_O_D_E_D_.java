ebacefa5447ef96236d19b99992574d2dbbeae04
Alan <a_barragan1@u.pacific.edu>
2019-11-02 20:48:34 -0700

    changed variable names for the sake of making our lives easier

--- src/projectFiles/H_A_R_D_C_O_D_E_D_.java
+++ src/projectFiles/H_A_R_D_C_O_D_E_D_.java
@@ -78,54 +78,54 @@ public class H_A_R_D_C_O_D_E_D_
 			Coordinates rock2 = new Coordinates (780,301);
 			Coordinates hole1 = new Coordinates (72,301);
 			Coordinates entry1 = new Coordinates (1150, 325);
-			Interactions rockOne = new Interactions(interactionType.obstacle_rock);
-			Interactions rockTwo = new Interactions(interactionType.obstacle_rock);
-			Interactions holeOne = new Interactions(interactionType.obstacle_hole);
-			Interactions entryOne = new Interactions(interactionType.entry_door);
-			map.setInteractions(rockOne, rock1, interactionStorage);
-			map.setInteractions(rockTwo, rock2, interactionStorage);
-			map.setInteractions(holeOne, hole1, interactionStorage);
-			map.setInteractions(entryOne, entry1, interactionStorage);
+			Interactions roomOneInteraction1 = new Interactions(interactionType.obstacle_rock);
+			Interactions roomOneInteraction2 = new Interactions(interactionType.obstacle_rock);
+			Interactions roomOneInteraction3 = new Interactions(interactionType.obstacle_hole);
+			Interactions roomOneInteraction4 = new Interactions(interactionType.entry_door);
+			map.setInteractions(roomOneInteraction1, rock1, interactionStorage);
+			map.setInteractions(roomOneInteraction2, rock2, interactionStorage);
+			map.setInteractions(roomOneInteraction3, hole1, interactionStorage);
+			map.setInteractions(roomOneInteraction4, entry1, interactionStorage);
 			}
 			
 			//Room 2
 			else if(userRoomPosition == "R2")
 			{
-			Coordinates rock3 = new Coordinates (80, 90);
-			Coordinates hole2 = new Coordinates (500,91);
-			Coordinates hole3 = new Coordinates (75, 500);
+			Coordinates rock1 = new Coordinates (80, 90);
+			Coordinates hole1 = new Coordinates (500,91);
+			Coordinates hole2 = new Coordinates (75, 500);
 			Coordinates enemy1 = new Coordinates (350, 76);
 			Coordinates enemy2 = new Coordinates (367, 504);
-			Coordinates entry2 = new Coordinates (0, 325);
-			Coordinates entry3 = new Coordinates (1150, 325);
-			Interactions rockThree = new Interactions(interactionType.obstacle_rock);
-			Interactions holeTwo = new Interactions(interactionType.obstacle_hole);
-			Interactions holeThree = new Interactions(interactionType.obstacle_hole);
-			Enemy enemyOne = new Enemy(1, 9, 6, 5, 4, 2, ElementType.EARTH);
-			Enemy enemyTwo = new Enemy(5,0,0,5,1,3, ElementType.FIRE);
-			Interactions entryTwo = new Interactions(interactionType.entry_door);
-			Interactions entryThree = new Interactions(interactionType.entry_door);
-			map.setInteractions(rockThree, rock3, interactionStorage);
-			map.setInteractions(holeTwo, hole2, interactionStorage);
-			map.setInteractions(holeThree, hole3, interactionStorage);
-			map.setEnemySpawn(enemyOne, enemy1, enemyStorage);
-			map.setEnemySpawn(enemyTwo, enemy2, enemyStorage);
-			map.setInteractions(entryTwo, entry2, interactionStorage);
-			map.setInteractions(entryThree, entry3, interactionStorage);
+			Coordinates entry1 = new Coordinates (0, 325);
+			Coordinates entry2 = new Coordinates (1150, 325);
+			Interactions roomTwoInteraction1 = new Interactions(interactionType.obstacle_rock);
+			Interactions roomTwoInteraction2 = new Interactions(interactionType.obstacle_hole);
+			Interactions roomTwoInteraction3 = new Interactions(interactionType.obstacle_hole);
+			Enemy roomTwoEnemy1 = new Enemy(1, 9, 6, 5, 4, 2, ElementType.EARTH);
+			Enemy roomTwoEnemy2 = new Enemy(5,0,0,5,1,3, ElementType.FIRE);
+			Interactions roomTwoInteraction4 = new Interactions(interactionType.entry_door);
+			Interactions roomTwoInteraction5 = new Interactions(interactionType.entry_door);
+			map.setInteractions(roomTwoInteraction1, rock1, interactionStorage);
+			map.setInteractions(roomTwoInteraction2, hole1, interactionStorage);
+			map.setInteractions(roomTwoInteraction3, hole2, interactionStorage);
+			map.setEnemySpawn(roomTwoEnemy1, enemy1, enemyStorage);
+			map.setEnemySpawn(roomTwoEnemy2, enemy2, enemyStorage);
+			map.setInteractions(roomTwoInteraction4, entry1, interactionStorage);
+			map.setInteractions(roomTwoInteraction5, entry2, interactionStorage);
 			}
 			
 			//Room 3
 			else if(userRoomPosition == "R3")
 			{
 			Coordinates stairs1 = new Coordinates (575,325);
-			Coordinates enemy3 = new Coordinates (800, 70);
-			Coordinates entry4 = new Coordinates (0, 325);
-			Interactions stairsOne = new Interactions(interactionType.entry_stair);
-			Enemy enemyThree = new Enemy(6,9,6,9,6,9, ElementType.WATER);
-			Interactions entryFour = new Interactions(interactionType.entry_door);
-			map.setInteractions(stairsOne, stairs1, interactionStorage);
-			map.setEnemySpawn(enemyThree, enemy3, enemyStorage);
-			map.setInteractions(entryFour, entry4, interactionStorage);
+			Coordinates enemy1 = new Coordinates (800, 70);
+			Coordinates entry1 = new Coordinates (0, 325);
+			Interactions roomThreeInteraction1 = new Interactions(interactionType.entry_stair);
+			Enemy roomThreeEnemy1 = new Enemy(6,9,6,9,6,9, ElementType.WATER);
+			Interactions roomThreeInteraction2 = new Interactions(interactionType.entry_door);
+			map.setInteractions(roomThreeInteraction1, stairs1, interactionStorage);
+			map.setEnemySpawn(roomThreeEnemy1, enemy1, enemyStorage);
+			map.setInteractions(roomThreeInteraction2, entry1, interactionStorage);
 			}
 		
 		}	
