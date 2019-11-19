76723fa8f7887f99c989ef9d3008d83ec0bd3733
Stan Lee <Stan Lee@10.15.45.141>
2019-11-18 18:12:45 -0800

    Made a move class for enemies

---
+++ src/starter/Move.java
@@ -0,0 +1,16 @@
+package starter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import starter.Enemy;
+
+public class Move implements ActionListener {
+	Enemy enem;
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
