452efd3f0cb02211ca9b2725d6409d754c23a0a2
Scharkey <jordanscharkey@gmail.com>
2019-11-21 15:22:34 -0800

    Functionality added to returnMenu button in death screen

--- src/starter/GameOverPane.java
+++ src/starter/GameOverPane.java
@@ -1,8 +1,13 @@
 package starter;
 
-import acm.graphics.GImage;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
 
-public class GameOverPane extends GraphicsPane{
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+
+public class GameOverPane extends GraphicsPane implements ActionListener {
 	private GImage gameOver;
 	private MainApplication program;
 	private GButton returnMenu;
@@ -12,17 +17,32 @@ public class GameOverPane extends GraphicsPane{
 		this.program = app;
 		gameOver = new GImage ("Game Over Screen.png",0,0);
 		gameOver.setSize(1150,650);
-		returnMenu = new GButton("Return to main menu", 400, 100, program.WINDOW_HEIGHT/2, 3*program.WINDOW_WIDTH/4);
+		returnMenu = new GButton("Return to main menu", program.WINDOW_WIDTH/2 - 75, program.WINDOW_HEIGHT - 150, 150, 50);
 	}
 	
 	@Override
 	public void showContents() {
 		program.add(gameOver);
+		program.add(returnMenu);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(gameOver);
+		program.remove(returnMenu);
+	}
+	
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == returnMenu) {
+			program.switchToTitleScreen();
+		}
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
