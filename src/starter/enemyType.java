055e7cb15b3e38e10a60b3a3ee523c8550c6b53c
Alan <a_barragan1@u.pacific.edu>
2019-11-22 00:22:20 -0800

    new enemy enums added to enemy type

--- src/starter/enemyType.java
+++ src/starter/enemyType.java
@@ -1,7 +1,9 @@
 package starter;
 
 public enum enemyType {
-	EARTHBat, FIREBat, WATERBat, EARTHSkull, FIRESkull, WATERSkull, rip;
+	EARTHBat, FIREBat, WATERBat, EARTHSkull, FIRESkull, 
+	WATERSkull, rip, EARTHSpider, WATERSpider, FIRESpider, EARTHSpooder,
+	WATERSpooder, FIRESpooder, EARTHDrawing, WATERDrawing, FIREDrawing;
 	
 	public String toString() {
 		switch(this) {
@@ -11,6 +13,16 @@ public enum enemyType {
 			case EARTHSkull: return "EARTHSkull";
 			case FIRESkull: return "FIRESkull";
 			case WATERSkull: return "WATERSkull";
+			case EARTHSpider: return "EARTHSpider";
+			case WATERSpider: return "WATERSpider";
+			case FIRESpider: return "FIRESpider";
+			case EARTHSpooder: return "EARTHSpooder";
+			case WATERSpooder: return "WATERSpooder";
+			case FIRESpooder: return "FIRESpooder";
+			case EARTHDrawing: return "EARTHDrawing";
+			case WATERDrawing: return "WATERDrawing";
+			case FIREDrawing: return "FIREDrawing";
+			
 			case rip: return "rip";
 		}
 		return "n/a";
