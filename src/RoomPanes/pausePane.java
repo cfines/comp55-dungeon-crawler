88f529d521712bd66d7dd99e2ded9a7e08f4b625
Scharkey <jordanscharkey@gmail.com>
2019-11-22 19:13:04 -0800

    Pause menu fully functional

--- src/RoomPanes/pausePane.java
+++ src/RoomPanes/pausePane.java
@@ -1,10 +1,12 @@
 package RoomPanes;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
+import acm.graphics.GLabel;
 import acm.graphics.GObject;
 import starter.GButton;
 import starter.GraphicsPane;
@@ -14,21 +16,27 @@ public class pausePane extends GraphicsPane implements ActionListener {
 	private MainApplication program;
 	//private GraphicsPane prevPane;
 	private GButton pauseButton;
+	private GLabel pauseLabel;
 	
 	public pausePane(MainApplication app){
 		this.program = app;
-		pauseButton = new GButton("Return", program.WINDOW_WIDTH/2 - 75, program.WINDOW_HEIGHT - 150, 150, 50);
+		pauseButton = new GButton("Return", program.WINDOW_WIDTH/2 - 75, program.WINDOW_HEIGHT - 130, 150, 50);
+		pauseLabel = new GLabel("PAUSED", program.WINDOW_WIDTH / 2 - 135 ,85);
+		pauseLabel.setColor(Color.red);
+		pauseLabel.setFont("Arial-Bold-64");
 	}
 
 	@Override
 	public void showContents() {
 		program.add(pauseButton);
+		program.add(pauseLabel);
 		
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(pauseButton);
+		program.remove(pauseLabel);
 		
 	}
 
