b280628aed6d54e213f0e3c27e60718cc2113b57
Scharkey <jordanscharkey@gmail.com>
2019-11-23 14:17:21 -0800

    Added exit button onto Q menu

--- src/RoomPanes/instructionsPane.java
+++ src/RoomPanes/instructionsPane.java
@@ -15,22 +15,30 @@ public class instructionsPane extends GraphicsPane implements ActionListener {
 	private MainApplication program;
 	private GRect background;
 	private Color color = new Color(213, 196, 161);
+	private GButton exit;
 	
 	public instructionsPane(MainApplication app) {
 		this.program = app;
+		
 		background = new GRect(program.WINDOW_WIDTH / 2 - 350, program.WINDOW_HEIGHT / 2 - 250, 700, 500);
 		background.setColor(color);
 		background.setFilled(true);
+		
+		exit = new GButton("X", program.WINDOW_WIDTH/2 + 300, program.WINDOW_HEIGHT/2 - 250, 50, 50);
+		exit.setFillColor(Color.red);
+		
 	}
 
 	@Override
 	public void showContents() {
 		program.add(background);
+		program.add(exit);
 	}
 
 	@Override
 	public void hideContents() {
 		program.remove(background);
+		program.remove(exit);
 	}
 
 	@Override
@@ -41,6 +49,13 @@ public class instructionsPane extends GraphicsPane implements ActionListener {
 	
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode() == KeyEvent.VK_Q) {
+			program.noLongerPaused();
+		}
+	}
+	
+	public void mouseReleased(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
+		if (obj == exit) {
 			program.noLongerPaused();
 		}
 	}
