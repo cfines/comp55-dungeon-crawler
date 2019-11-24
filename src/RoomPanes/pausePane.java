26663cd7f34bf9eb394face33f415ddae57744a9
Scharkey <jordanscharkey@gmail.com>
2019-11-24 02:31:20 -0800

    Created variables for developer mode pause menu

--- src/RoomPanes/pausePane.java
+++ src/RoomPanes/pausePane.java
@@ -16,6 +16,35 @@ public class pausePane extends GraphicsPane implements ActionListener {
 	private GButton resumeButton;
 	private GLabel pauseLabel;
 	
+	///////DEVELOPER_MODE_LABELS_AND_BUTTONS/////////////////
+	private GLabel baseFloor;
+	private GLabel osvaldoomFloor;
+	private GLabel shadowRealm;
+	private GLabel lucinaDomain;
+	private GLabel schtanlyParable;
+	private GLabel bombFloor;
+	private GButton BF_R1;
+	private GButton BF_RKey;
+	private GButton BF_R8;
+	private GButton BF_Boss;
+	private GButton OF_R1;
+	private GButton OF_RKey;
+	private GButton OF_Boss;
+	private GButton SR_R1;
+	private GButton SR_RKey;
+	private GButton SR_RBoss;
+	private GButton LD_R1;
+	private GButton LD_RKey;
+	private GButton LD_RBoss;
+	private GButton SP_R1;
+	private GButton SP_RKey;
+	private GButton SP_Boss;
+	private GButton BOMB_R1;
+	private GButton BOMB_Bomb1;
+	private GButton BOMB_Bomb2;
+	private GButton BOMB_Bomb3;
+	
+	
 	public pausePane(MainApplication app){
 		this.program = app;
 		pauseButton = new GButton("Quit", program.WINDOW_WIDTH/2 - 75, program.WINDOW_HEIGHT - 130, 150, 50);
@@ -23,6 +52,7 @@ public class pausePane extends GraphicsPane implements ActionListener {
 		pauseLabel = new GLabel("PAUSED", program.WINDOW_WIDTH / 2 - 135 ,85);
 		pauseLabel.setColor(Color.red);
 		pauseLabel.setFont("Arial-Bold-64");
+		
 	}
 
 	@Override
@@ -30,6 +60,11 @@ public class pausePane extends GraphicsPane implements ActionListener {
 		program.add(pauseButton);
 		program.add(pauseLabel);
 		program.add(resumeButton);
+		
+		if(program.getDeveloperMode()) {
+			
+		}
+		
 	}
 
 	@Override
@@ -37,6 +72,11 @@ public class pausePane extends GraphicsPane implements ActionListener {
 		program.remove(pauseButton);
 		program.remove(pauseLabel);
 		program.remove(resumeButton);
+		
+		if(program.getDeveloperMode()) {
+			
+		}
+		
 	}
 
 	@Override
