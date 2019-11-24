493e9559a76015dce38a4f818e2f689c92790a6b
Stanley <stanyu123@gmail.com>
2019-11-23 18:49:05 -0800

    Created new set of rooms

--- src/StanFloor/stan_R1.java
+++ src/StanFloor/stan_R1.java
@@ -1,5 +1,24 @@
 package StanFloor;
 
-public class stan_R1 {
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
+import javax.swing.Timer;
+
+import RoomPanes.GraphicsPane;
+import RoomPanes.KeyPressedManager;
+import RoomPanes.MainApplication;
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import enemyInteraction.Enemy;
+import enemyInteraction.Interactions;
+import enemyInteraction.interactionType;
+import removeLater.User;
+public class stan_R1 {
+	
 }
