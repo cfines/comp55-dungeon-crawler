c50fd9f63ba201d7cef5e10414643b65623a014c
Alan <a_barragan1@u.pacific.edu>
2019-11-24 04:51:35 -0800

    testing combat for a magic mf

--- src/FyiahEmburemFloor/fe_R3.java
+++ src/FyiahEmburemFloor/fe_R3.java
@@ -22,7 +22,7 @@ import removeLater.User;
 
 public class fe_R3 extends GraphicsPane implements ActionListener{
 	private MainApplication program;
-	private GImage enemy1, enemy2, tree1, tree2, E4, E5, background,userRep, userWeapon;
+	private GImage enemy1, tree1, tree2, E4, E5, background,userRep, userWeapon;
 	
 	private int degree;
 	private int timerCont = 0;
@@ -30,24 +30,33 @@ public class fe_R3 extends GraphicsPane implements ActionListener{
 	private Timer t = new Timer(30, this);
 	private boolean move = true;
 	private boolean atkUp,atkLeft,atkDown,atkRight;
+	private GImage badGuy = new GImage ("Dark Mage.gif",575,70);
 	private GRect voidSpace;
 	private ArrayList<Enemy> listOfEnemies = new ArrayList<Enemy>();
 	private ArrayList<Interactions> listOfInter = new ArrayList<Interactions>();
 	private ArrayList<GImage> enemyImages = new ArrayList<GImage>();
 	private ArrayList<GImage> elements = new ArrayList<GImage>();
 	private KeyPressedManager mover;
+	private boolean attack = false, hit = false;
+	private Enemy magic = new Enemy(100,100,99999,1,0,0,ElementType.FIRE, enemyType.Nagic);
+	private ArrayList<Enemy> listOfProjectiles = new ArrayList<Enemy>();
+	private Enemy DarkMage = new Enemy(2,2,2,2,575,70, ElementType.FIRE, enemyType.DarkMage);
 	
 	public fe_R3(MainApplication app) 
 	{
 		this.program = app;
 		user = program.getUser();
-		Enemy ienemy1 = new Enemy(2,2,2,2,900,100, ElementType.FIRE, enemyType.Samurai);
-		Enemy ienemy2 = new Enemy(2,2,2,2,200,100, ElementType.FIRE, enemyType.DarkMage);
+		Enemy ienemy1 = new Enemy(10,10,2,2,900,100, ElementType.FIRE, enemyType.Samurai);
+		
 		Interactions iE4 = new Interactions(interactionType.entry_door_SOUTH,575,535);
 		Interactions iE5 = new Interactions(interactionType.entry_door_NORTH,575,30);
+		Interactions itree1 = new Interactions(interactionType.treeFell,325,59);
+		Interactions itree2 = new Interactions(interactionType.tree,525,200);
 		
+		badGuy = DarkMage.getImage();
+		tree1 = itree1.getImage();
+		tree2 = itree2.getImage();
 		enemy1 = ienemy1.getImage();
-		enemy2 = ienemy2.getImage();
 		E4 = iE4.getImage();
 		E5 = iE5.getImage();
 		background = new GImage("FE Forest Clearing.png",15,30);
@@ -60,16 +69,21 @@ public class fe_R3 extends GraphicsPane implements ActionListener{
 		voidSpace.setFilled(true);
 		
 		listOfEnemies.add(ienemy1);
-		listOfEnemies.add(ienemy2);
+		listOfEnemies.add(DarkMage);
 		listOfInter.add(iE4);
 		listOfInter.add(iE5);
+		listOfInter.add(itree1);
+		listOfInter.add(itree2);
 		
 		elements.add(background);
+		
 		elements.add(E4);
 		elements.add(E5);
-		
+		elements.add(tree1);
+		elements.add(tree2);
+		enemyImages.add(badGuy);
 		enemyImages.add(enemy1);
-		enemyImages.add(enemy2);
+		elements.add(userRep);
 		
 		mover = new KeyPressedManager(program, user, userRep, listOfEnemies, listOfInter, elements,
 				atkUp, atkLeft, atkRight, atkDown, userWeapon);
@@ -123,6 +137,15 @@ public class fe_R3 extends GraphicsPane implements ActionListener{
 	public void enemyMovement() {
 		if(everyXSeconds(20)) {
 			move = !move;
+			attack = !attack;
+			if(attack) {
+				magic = new Enemy(100, 100, 2, 2, (int)DarkMage.getCoordX(), (int)DarkMage.getCoordY() + 75, ElementType.FIRE, enemyType.projectile);
+				listOfProjectiles.add(magic);
+			} else {
+				listOfProjectiles.remove(magic);
+				program.remove(magic.getImage());
+				hit = false;
+			}
 		}
 		for (Enemy enem : listOfEnemies) {
 			degree+=5;
@@ -136,17 +159,35 @@ public class fe_R3 extends GraphicsPane implements ActionListener{
 				enem.getImage().move(-moveX, -moveY);
 			}
 			else if(enem.getEnemyType() == enemyType.DarkMage) {
-				enem.getImage().movePolar(100, degree);
+				enem.getImage().movePolar(1, degree);
 			}
 			enem.setStartX(enem.getImage().getX());
 			enem.setStartY(enem.getImage().getY());
 			}
+		if(listOfProjectiles.size() >= 1) {
+			for(Enemy arr : listOfProjectiles) {
+				arr.getEnemyStats().setCoordX(DarkMage.getCoordX());
+				arr.getEnemyStats().setCoordY(DarkMage.getCoordY());
+				
+				if(checkHitBack(arr, userWeapon) && atkUp) { 
+					hit = true; 
+				}
+				program.add(arr.getImage());
+				
+				if(hit) { 
+					arr.getImage().move(0, -10);
+				} 
+				else { 	
+					arr.getImage().move(0, 10); 
+				}
+			}
 		}
+	}
 	
 	public void deleteEnemy() {
 		mover.setDeleteEnemy(false);
 		for(int i = 0; i < listOfEnemies.size(); i++) {
-			if(listOfEnemies.get(i).getEnemyType() == enemyType.rip) {
+			if(listOfEnemies.get(i).getEnemyType() == enemyType.Nagic) {
 				enemyImages.remove(i);
 				listOfEnemies.remove(i);
 			} else {
@@ -169,7 +210,7 @@ public class fe_R3 extends GraphicsPane implements ActionListener{
 			user.setX(575);
 			user.setY(410);
 			userRep.setLocation(user.getX(), user.getY());
-			//program.switchToR4();
+			program.switchToR4();
 		}
 	}
 	
@@ -184,5 +225,22 @@ public class fe_R3 extends GraphicsPane implements ActionListener{
 	@Override
 	public void keyReleased(KeyEvent e) { 
 	mover.notReallyKeyReleased(e);
+	}
+	
+	public boolean checkHitBack(Enemy enem, GImage image) {
+		return (enem.getImage().getY() - image.getY() <= 60
+				&& enem.getImage().getY() - image.getY() >= -60
+				&& enem.getImage().getX() - image.getX() <= 60
+				&& enem.getImage().getX() - image.getX() >= -60);
+	}
+	
+	public void userCombat() {
+		for(int i = 0; i < listOfProjectiles.size(); i++) {
+			if(checkHitBack(listOfProjectiles.get(i), DarkMage.getImage())) { 
+				DarkMage.getEnemyStats().setHP_cur(0);
+				listOfProjectiles.remove(listOfProjectiles.get(i));
+				program.remove(magic.getImage());
+			}
+		}
 	}
 }
