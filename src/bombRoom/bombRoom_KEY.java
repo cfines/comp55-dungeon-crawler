4dd4c4d04383cb729522fd93a64fff7674b1e087
Scharkey <jordanscharkey@gmail.com>
2019-11-24 16:27:14 -0800

    traversal through bomb_key (and whole floor) successful

--- src/bombRoom/bombRoom_KEY.java
+++ src/bombRoom/bombRoom_KEY.java
@@ -1,25 +1,164 @@
 package bombRoom;
 
-import RoomPanes.GraphicsPane;
-import RoomPanes.MainApplication;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.MouseEvent;
+import java.util.ArrayList;
 
-public class bombRoom_KEY extends GraphicsPane {
+import javax.swing.Timer;
+
+import RoomPanes.GraphicsPane;
+import RoomPanes.KeyPressedManager;
+import RoomPanes.MainApplication;
+import acm.graphics.GImage;
+import acm.graphics.GObject;
+import acm.graphics.GRect;
+import enemyInteraction.ElementType;
+import enemyInteraction.Enemy;
+import enemyInteraction.Interactions;
+import enemyInteraction.enemyType;
+import enemyInteraction.interactionType;
+import removeLater.User;
+
+
+public class bombRoom_KEY extends GraphicsPane implements ActionListener {
 	private MainApplication program;
-	
+	private GImage rock1, rock2, hole1, EN, ES, EE, EW, background, userRep, userWeapon;
+	private ArrayList<GImage> elements = new ArrayList<GImage>();
+	private ArrayList<Enemy> listOfEnemies = new ArrayList<Enemy>();
+	private ArrayList<GImage> enemyImages = new ArrayList<GImage>();
+	private ArrayList<Interactions> listOfInter = new ArrayList<Interactions>();
+	private GRect voidSpace;
+	private User user;
+	private boolean atkUp,atkDown,atkLeft,atkRight;
+	Timer t = new Timer(30, this);
+	private int decrementTimer = 0;
+	private boolean unlocked = false;
+
+	private KeyPressedManager mover;
+
 	public bombRoom_KEY(MainApplication app) {
 		this.program = app;
+		user = program.getUser(); 
+		Interactions oE4 = new Interactions(interactionType.entry_door_WEST,27,300);
+
+		listOfInter.add(oE4);
+
+		background = new GImage("Base_Floor (Regular Floor).png", 15,30);
+		
+		EW = oE4.getImage();
+
+		userRep = new GImage("Rogue_(Sample User).gif");
+		userWeapon = new GImage("Fire Sword(RIGHT).png", 0, 0);
+
+		voidSpace = new GRect(0,0);
+		voidSpace.setSize(1150,650);
+		voidSpace.setColor(Color.BLACK);
+		voidSpace.setFilled(true);
+
+		elements.add(background);
+		elements.add(EW);
+		elements.add(userRep);
+
+		mover = new KeyPressedManager(program, user, userRep, listOfEnemies, listOfInter, elements,
+				atkUp, atkLeft, atkRight, atkDown, userWeapon);
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		decrementTimer();
+		if(mover.getDeleteEnemy()) { deleteEnemy(); }
+		mover.notReallyActionPerformed(e);
+		nextRoom();
+		userRep.setLocation(user.getX(), user.getY());
 	}
 
 	@Override
 	public void showContents() {
-		// TODO Auto-generated method stub
+		t.start();
+		program.add(voidSpace);
+		for (int i = 0; i <= elements.size() - 1; i++) {
+			program.add(elements.get(i));
+		}
 		
+		if(listOfEnemies.size() >= 1) {
+			for(int i = 0; i < enemyImages.size(); i++) {
+				if(listOfEnemies.get(i).getEnemyType() == enemyType.rip) {
+					enemyImages.remove(i);
+					listOfEnemies.remove(i);
+				} else {
+					program.add(enemyImages.get(i));
+				}
+			}
+		}
+		
+		program.bombOverlay();
+		program.drawOverlay(5, program.getFloorNum());
 	}
 
 	@Override
 	public void hideContents() {
-		// TODO Auto-generated method stub
+		t.stop();
+		program.remove(voidSpace);
+		for (int i = 0; i <= elements.size() - 1; i++) {
+			program.remove(elements.get(i));
+		}
+		program.refreshOverlay();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		GObject obj = program.getElementAt(e.getX(), e.getY());
 		
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if((e.getKeyCode() == KeyEvent.VK_ESCAPE) || (e.getKeyCode() == KeyEvent.VK_Q)) {
+			t.stop();
+		}
+		mover.notReallyKeyPressed(e);
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		mover.notReallyKeyReleased(e);
+	}
+	
+	public void decrementTimer() {
+		decrementTimer++;
+		if(decrementTimer == 40 && !program.getBombsDeactivated()) {
+			decrementTimer = 0;
+			program.setBombCounter(program.getBombCounter() - 1);
+			program.bombOverlay();
+		}
+	}
+	
+	public void deleteEnemy() {
+		mover.setDeleteEnemy(false);
+		for(int i = 0; i < listOfEnemies.size(); i++) {
+			if(listOfEnemies.get(i).getEnemyType() == enemyType.rip) {
+				enemyImages.remove(i);
+				listOfEnemies.remove(i);
+			} else {
+				program.add(enemyImages.get(i));
+			}
+		}
+	}
+	
+	private void nextRoom() {
+		double userX = userRep.getX();
+		double userY = userRep.getY();
+		double userX2 = userRep.getX() + 80;
+		double userY2 = userRep.getY() + 80;
+		if(userX >= EW.getX() && userY >= EW.getY() && userX <= EW.getX() + 75 && userY <= EW.getY() + 75) {
+			user.setX(900);
+			user.setY(300);
+			userRep.setLocation(user.getX(), user.getY());
+			program.switchToBombRoomR3();
+		}
 	}
 	
 }
