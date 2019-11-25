87761e6b715d0a1bd5055dcbceb4fa392987063a
Alan <a_barragan1@u.pacific.edu>
2019-11-24 18:12:28 -0800

    adding enemies to enemy type and enemy

--- src/enemyInteraction/Enemy.java
+++ src/enemyInteraction/Enemy.java
@@ -61,7 +61,8 @@ public class Enemy {
 				type == enemyType.WATERCheese || type == enemyType.FIRECheese || type == enemyType.EARTHFish || type == enemyType.WATERFish || 
 				type == enemyType.FIREFish || type == enemyType.leg || type == enemyType.goon1 || type == enemyType.goon2 || type == enemyType.insidePacific || 
 				type == enemyType.momoko || type == enemyType.Duma || type == enemyType.Brigand||
-				type == enemyType.Samurai || type == enemyType.DarkMage || type == enemyType.face) {
+				type == enemyType.Samurai || type == enemyType.DarkMage || type == enemyType.face || type == enemyType.Berkut || type == enemyType.Rinea
+	|| type == enemyType.Deadass || type == enemyType.Dread) {
 			image = new GImage(type + ".gif", dx, dy);
 		} else if(type == enemyType.electric) {
 			image = new GImage(type + ".jpg", dx, dy);
