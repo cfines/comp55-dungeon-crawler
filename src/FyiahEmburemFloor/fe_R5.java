2724ab64bb56079b907c24b1066fd89f67b00e3f
Alan <a_barragan1@u.pacific.edu>
2019-11-24 17:02:01 -0800

    adding rock wall

--- src/FyiahEmburemFloor/fe_R5.java
+++ src/FyiahEmburemFloor/fe_R5.java
@@ -1,17 +1,55 @@
 package FyiahEmburemFloor;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.Timer;
 
 import RoomPanes.GraphicsPane;
+import RoomPanes.KeyPressedManager;
 import RoomPanes.MainApplication;
+import acm.graphics.GImage;
+import acm.graphics.GRect;
+import enemyInteraction.ElementType;
+import enemyInteraction.Enemy;
+import enemyInteraction.Interactions;
+import enemyInteraction.enemyType;
+import removeLater.User;
 
 public class fe_R5 extends GraphicsPane implements ActionListener{
 	private MainApplication program;
+	private GImage enemy1, enemy2, tree1, tree2, tree3, tree4, tree5, E2, E3, background,userRep, userWeapon;
+	private ArrayList<GImage> enemyImages = new ArrayList<GImage>();
+	private ArrayList<GImage> elements = new ArrayList<GImage>();
+	private GRect voidSpace;
+	private ArrayList<Enemy> listOfEnemies = new ArrayList<Enemy>();
+	private ArrayList<Interactions> listOfInter = new ArrayList<Interactions>();
+	private boolean atkUp,atkLeft,atkDown,atkRight;
+	private int degree;
+	private int timerCont = 0;
+	private User user;
+	private Timer t = new Timer(30, this);
+	private boolean move = true;
+	private KeyPressedManager mover;
 	
 	public fe_R5(MainApplication app) 
 	{
+		this.program = app;
+		user = program.getUser();
+		background = new GImage("FE Rock Room.png", 15,30);
+		userRep = new GImage("Rogue_(Sample User).gif", 0, 0);
+		userWeapon = new GImage("Fire Sword(RIGHT).png", 0, 0);
+		voidSpace = new GRect(0,0);
+		voidSpace.setSize(1150,650);
+		voidSpace.setColor(Color.BLACK);
+		voidSpace.setFilled(true);
 		
+		elements.add(background);
+		
+		mover = new KeyPressedManager(program, user, userRep, listOfEnemies, listOfInter, elements,
+				atkUp, atkLeft, atkRight, atkDown, userWeapon);
 	}
 	@Override
 	public void actionPerformed(ActionEvent e) {
