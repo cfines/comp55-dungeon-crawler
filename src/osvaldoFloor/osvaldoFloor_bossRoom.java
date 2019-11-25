f1b9a0cfcf5bbd489ebe4289a463b1cf9dceca9e
Scharkey <jordanscharkey@gmail.com>
2019-11-24 17:09:51 -0800

    Fixed projectile error with godmode

--- src/osvaldoFloor/osvaldoFloor_bossRoom.java
+++ src/osvaldoFloor/osvaldoFloor_bossRoom.java
@@ -291,6 +291,7 @@ public class osvaldoFloor_bossRoom extends GraphicsPane implements ActionListene
 	}
 	
 	public void enemyCombat() {
+		if(program.getUser().getInvincibility()) {return;}
 		for(int i = 0; i < listOfProjectiles.size(); i++) {
 			if(checkHitBack(listOfProjectiles.get(i), userRep)) { 
 				int newHealth = program.getUser().getUserStats().getHP_cur() - 1;
